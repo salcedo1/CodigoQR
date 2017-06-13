8de3ee750bae9944cbdda74d4607bb028fd6a5e2
ivan <abrasalc2012@hotmail.com>
2017-06-13 11:42:19 -0500

    avance

---
+++ proyecto/index.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Documento sin t&iacute;tulo</title>
+<style type="text/css">
+<!--
+#cuerpo {
+	position:absolute;
+	width:1343px;
+	height:1800px;
+	z-index:0;
+}
+#izquirda {
+	position:absolute;
+	width:50px;
+	height:1800px;
+	z-index:0;
+	background-color:#006633;
+}
+#cabeza {
+	position:absolute;
+	width:1190px;
+	height:175px;
+	z-index:2;
+	left: 50px;
+	top: 0px;
+}
+#cabizquierda {
+	position:absolute;
+	width:50px;
+	height:175px;
+	z-index:3;
+	left: 1240px;
+	top: 0px;
+	background-color:#99CC00;
+}
+#derecha {
+	position:absolute;
+	width:50px;
+	height:1800px;
+	z-index:4;
+	left: 1290px;
+	top: 0px;
+	background-color:#006633;
+}
+#cuello {
+	position:absolute;
+	width:880px;
+	height:50px;
+	z-index:5;
+	left: 50px;
+	top: 175px;
+}
+#dere {
+	position:absolute;
+	width:307px;
+	height:1625px;
+	z-index:6;
+	left: 930px;
+	top: 175px;
+}
+#cuerpoinfe {
+	position:absolute;
+	width:880px;
+	height:1575px;
+	z-index:7;
+	left: 50px;
+	top: 225px;
+}
+#dereinfer {
+	position:absolute;
+	width:52px;
+	height:1625px;
+	z-index:8;
+	left: 1238px;
+	top: 175px;
+	background-color:#FFFF00;
+}
+.Estilo1 {font-size: 24px}
+-->
+</style>
+</head>
+
+<body>
+<div id="cuerpo">
+  <div id="izquirda"></div>
+  <div id="cabeza"></div>
+  <div id="cabizquierda"></div>
+  <div id="derecha"></div>
+  <div id="cuello">
+    <div align="center" class="Estilo1">ZONAS TORISTICAS DE ANDAHUAYLAS </div>
+  </div>
+  <div id="dere">
+  <h1>ANDAHUAYLAS</h1>
+  
+  <P align="justify">La ciudad de Andahuaylas es capital de la provincia de del mismo nombre, es una de las 7 provincias que conforman el Departamento de Apurimac. Es tambien conocida como la tierra de los celajes (por el color del cielo al atardecer) y la cultura CHANKA.
+
+La ciudad se encuentra sobre los 2,980 metros sobre el nivel del mar y posee un clima ligeramente frio. En su Plaza de Armas destaca la Pileta construida de una sola piedra y la Catedral de San Pedro que fue construida en la Epoca Colonial.</P>
+  <P align="justify"> A continuacion un resumen de los principales atractivos que encontrara en esta provincia del departamento de Apurimac, los cuales podran apreciar con mas detalle en las siguientes paginas. </P>
+  </div>
+  <div id="cuerpoinfe"></div>
+  <div id="dereinfer"></div>
+</div>
+</body>
+</html>
